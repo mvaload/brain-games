f2233fa9bc74d871bdc3af067c2fe29f22f7f89b
Moiseenko Vladimir <mvaload@gmail.com>
2018-07-24 15:09:11 +0300

    add namespace

--- src/Cli.php
+++ src/Cli.php
@@ -1,7 +1,12 @@
 <?
 
+namespace BrainGames\Cli;
+
 use function \cli\line;
 
-line('Welcome to the Brain Games!');
-$name = \cli\prompt('May I have your name?');
-line("Hello, %s!", $name);
+function run()
+{
+    line('Welcome to the Brain Games!');
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
