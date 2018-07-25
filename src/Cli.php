de1282822d8779e9f75a0ab38dee5cb2e1c93c9b
Moiseenko Vladimir <mvaload@gmail.com>
2018-07-25 10:37:20 +0300

    changes structure

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,26 @@ namespace BrainGames\Cli;
 use function \cli\line;
 use function \cli\prompt;
 
-function run()
+function playGame($description, $question, $correctAnswer)
 {
     line('Welcome to the Brain Games!');
+    line($description . PHP_EOL);
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+
+    for ($i = 0; $i < 3; $i++) {
+        $quest = $question();
+        line("Question: %s", $quest);
+        $answer = prompt("Your answer", 0);
+        $rightAnswer = $correctAnswer($quest);
+        if ($rightAnswer == $answer) {
+            line("Correct!");
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $rightAnswer);
+            line("Let's try again, %s!", $name);
+            return;
+        }
+    }
+    line("Congratulations, %s!", $name);
+    return;
 }
